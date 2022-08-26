8ae2632c5811761915f0ad2bc31290abbe39c71d
basikaka <hellochenpro@hotmail.com>
2022-08-26 16:09:57 +0800

    清除UserRepository中多余的方法

--- EstateBack/src/main/java/com/hongrider/repository/UserRepository.java
+++ EstateBack/src/main/java/com/hongrider/repository/UserRepository.java
@@ -4,7 +4,5 @@ import com.hongrider.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-   public  default String greet(){
-      return "Hello, World";
-   }
+
 }
