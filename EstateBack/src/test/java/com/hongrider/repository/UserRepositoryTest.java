71cf70acb30ba0fb3276475329c1f90d647162c3
basikaka <hellochenpro@hotmail.com>
2022-08-27 13:11:15 +0800

    测试数据库与开发数据库相同

--- EstateBack/src/test/java/com/hongrider/repository/UserRepositoryTest.java
+++ EstateBack/src/test/java/com/hongrider/repository/UserRepositoryTest.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,13 +19,18 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    void findAll(){
-        System.out.println(userRepository.findAll());
+    void findAll() {
+//        System.out.println(userRepository.findAll());
+        User first_user = new User(1, "harry", "小名",
+                "harry@gamil.com", "1111111",
+                "业主", "富力半岛");
+        Iterable<User> users = userRepository.findAll();
+        assertThat(users).hasSize(14).first().isEqualTo(first_user);
     }
 
     @Test
     @Transactional
-    void save(){
+    void save() {
         User user = new User();
         user.setName("shiny");
         user.setAlias("璇妮");
@@ -30,28 +39,49 @@ class UserRepositoryTest {
         user.setEmail("shiny@qq.com");
         user.setPassword("111111");
 
-        User savedUser = userRepository.save( user );
-        System.out.println("测试保存：" + savedUser);
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).hasFieldOrPropertyWithValue("name", "shiny");
+        assertThat(savedUser).hasFieldOrPropertyWithValue("estate", "珠江新城");
+        assertThat(savedUser).hasFieldOrPropertyWithValue("email", "shiny@qq.com");
+
+//        System.out.println("测试保存：" + savedUser);
     }
 
     @Test
     void findById() {
-        System.out.println( userRepository.findById( 9) );
+        //System.out.println( userRepository.findById( 9) );
+
+        User first_user = new User(1, "harry", "小名",
+                "harry@gamil.com", "1111111",
+                "业主", "富力半岛");
+
+        User user = userRepository.findById(1).get();
+        assertThat(user).isEqualTo(first_user);
     }
 
     @Test
     @Transactional
-    void update(){
-        User user = userRepository.findById( 5 ).get();
-        user.setEstate( "南国花园");
-        userRepository.save( user);
-        System.out.println(user);
+    void update() {
+        User user = userRepository.findById(5).get();
+        user.setEstate("南国花园");
+        user.setRole("租客");
+        User updatedUser = userRepository.save(user);
+        assertThat(updatedUser).hasFieldOrPropertyWithValue("estate", "南国花园");
+        assertThat(updatedUser).hasFieldOrPropertyWithValue("role", "租客");
+//        System.out.println(user);
     }
 
     @Test
     @Transactional
-    void delete(){
+    void delete() {
+        List<User> users = userRepository.findAll();
+        Integer befordel = users.size();
         userRepository.deleteById(4);
+        users = userRepository.findAll();
+        Integer afterdel = befordel - users.size();
+
+        assertThat(afterdel).isEqualTo(1);
     }
 
 }
